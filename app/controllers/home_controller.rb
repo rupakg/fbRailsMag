9fb1e3621be0ee4ad10b311a858f5aa68e0acd77
Rupak Ganguly <rupakg@gmail.com>
2009-05-03 23:18:47 -0400

    added the blueprint.css framework and set the basic layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
-#  ensure_authenticated_to_facebook :set_user
-#  before_filter :get_user
-#
-#  def get_user
-#    @facebook_user = facebook_session.user
-#    @user = User.find_or_create_by_facebook_uid(@facebook_user.uid)
-#  end
  
   def index
     respond_to do |format|
@@ -14,7 +7,4 @@ class HomeController < ApplicationController
     end
   end
 
-  def debug
-    #render_with_facebook_debug_panel
-  end
 end
